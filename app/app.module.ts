8e5a62c3cb686dad5e6daa3ad10731455e6f6570
Ramzan Dieze <ramzandieze35@gmail.com>
2017-04-16 14:01:50 +0530

    WebApp
    WebApp using Angular2 and Firebase

---
+++ app/app.module.ts
@@ -0,0 +1,60 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
+import { AngularFireModule, AuthProviders, AuthMethods} from 'angularfire2';
+import {FirebaseService} from './services/firebase.service';
+import {FlashMessagesModule} from 'angular2-flash-messages';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListingsComponent } from './components/listings/listings.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListingComponent } from './components/listing/listing.component';
+import { AddListingComponent } from './components/add-listing/add-listing.component';
+import { EditListingComponent } from './components/edit-listing/edit-listing.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCCL85EytjV5nsYkPUapIa3xvTOBfC2DQg",
+    authDomain: "dentacare-9e5f6.firebaseapp.com",
+    databaseURL: "https://dentacare-9e5f6.firebaseio.com",
+    projectId: "dentacare-9e5f6",
+    storageBucket: "dentacare-9e5f6.appspot.com",
+    messagingSenderId: "864616280720"
+};
+
+const firebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Popup
+};
+
+const appRoutes: Routes = [
+  {path:'', component:HomeComponent},
+  {path: 'listings', component:ListingsComponent},
+  {path:'listing/:id', component:ListingComponent},
+  {path: 'add-listing', component:AddListingComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ListingsComponent,
+    NavbarComponent,
+    ListingComponent,
+    AddListingComponent,
+    EditListingComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    FlashMessagesModule,
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig),
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [FirebaseService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app/components/listing/listing.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import {FirebaseService} from '../../services/firebase.service';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import { AngularFire} from 'angularfire2';
+
+
+@Component({
+  selector: 'app-listing',
+  templateUrl: './listing.component.html',
+  styleUrls: ['./listing.component.css']
+})
+export class ListingComponent implements OnInit {
+  id:any;
+  listing: any;
+  imageUrl:any;
+
+  constructor(
+    private firebaseService: FirebaseService,
+    private router:Router,
+    private route:ActivatedRoute,
+    public af: AngularFire
+  ) { 
+    this.af.auth.subscribe(auth => {
+     //Here you get the user information
+     console.log("My auth : "+auth.uid);
+    })
+  }
+
+  ngOnInit() {
+    // Get ID
+    this.id = this.route.snapshot.params['id'];
+
+    this.firebaseService.getListingDetails(this.id).subscribe(listing => {
+      this.listing = listing;
+      console.log(listing);
+
+      // @TODO - Storage Ref
+    });
+
+    // console.log(firebase.auth().currentUser.uid);
+
+  }
+
+}
